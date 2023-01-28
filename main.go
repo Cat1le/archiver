88f1e64a35d3d1c3fb685c697579e7f4c0452371
Nikita <nikitaserov912@gmail.com>
2023-01-29 02:01:08 +0300

    init

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"archiver/storage"
+	"bytes"
+	"errors"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/utils"
+	"mime"
+	"mime/multipart"
+)
+
+const (
+	FileMaxSize         = 1024 * 1024 * 1024
+	FileMaxSizeReadable = "1GB"
+	StorageDirectory    = "archiver"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{Immutable: true})
+	app.Use(cors.New(cors.Config{AllowCredentials: true}))
+	app.Use(logger.New())
+	storageFactory := storage.New(StorageDirectory)
+	app.Get("/begin", func(ctx *fiber.Ctx) error {
+		return ctx.SendString(utils.UUIDv4())
+	})
+	app.Post("/upload/:id", func(ctx *fiber.Ctx) error {
+		store := storageFactory.Session(ctx.Params("id", ""))
+		if store.StatusCode() != storage.CodeWaiting {
+			return errors.New("archive already processing")
+		}
+		form, err := readForm(ctx)
+		if err != nil {
+			return err
+		}
+		file, err := extractFile(form)
+		if err != nil {
+			return err
+		}
+		if file.Size > FileMaxSize {
+			return errors.New("file size is bigger than " + FileMaxSizeReadable)
+		}
+		store.Create(file)
+		err = ctx.SendString("Successfully uploaded")
+		if err != nil {
+			panic(err)
+		}
+		return nil
+	})
+	app.Get("/delete/:id/:file", func(ctx *fiber.Ctx) error {
+		store := storageFactory.Session(ctx.Params("id", ""))
+		if store.StatusCode() != storage.CodeWaiting {
+			return errors.New("archive already processing")
+		}
+		file := ctx.Params("file", "")
+		if file == "" {
+			return errors.New("file is empty")
+		}
+		return store.Delete(file)
+	})
+	app.Get("/zip/:id", func(ctx *fiber.Ctx) error {
+		store := storageFactory.Session(ctx.Params("id", ""))
+		if store.StatusCode() == storage.CodeWaiting {
+			go store.Zip()
+		}
+		err := ctx.JSON(map[string]any{
+			"code":     store.StatusCode(),
+			"progress": store.StatusProgress(),
+		})
+		if err != nil {
+			panic(err)
+		}
+		return nil
+	})
+	app.Get("/download/:id", func(ctx *fiber.Ctx) error {
+		store := storageFactory.Session(ctx.Params("id", ""))
+		if store.StatusCode() != storage.CodeFinished {
+			return errors.New("archive haven't done")
+		}
+		defer store.Reset()
+		return ctx.SendFile(store.ZipPath())
+	})
+	err := app.Listen("0.0.0.0:8080")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readForm(ctx *fiber.Ctx) (*multipart.Form, error) {
+	_, params, err := mime.ParseMediaType(ctx.GetReqHeaders()["Content-Type"])
+	if err != nil {
+		return nil, err
+	}
+	bodyReader := bytes.NewReader(ctx.Body())
+	multipartReader := multipart.NewReader(bodyReader, params["boundary"])
+	return multipartReader.ReadForm(1024)
+}
+
+func extractFile(form *multipart.Form) (*multipart.FileHeader, error) {
+	files, ok := form.File["file"]
+	if !ok {
+		return nil, errors.New("file field not present")
+	}
+	if len(files) != 1 {
+		return nil, errors.New("only one file per request")
+	}
+	return files[0], nil
+}
---
+++ storage/storage.go
@@ -0,0 +1,192 @@
+package storage
+
+import (
+	"archive/zip"
+	"errors"
+	"io"
+	"mime/multipart"
+	"os"
+	"path"
+)
+
+const (
+	CodeWaiting = iota
+	CodeRunning
+	CodeFinished
+)
+
+type Partial struct {
+	statuses map[string]*Storage
+	dir      string
+}
+
+type Storage struct {
+	dir, session, zip string
+	status            Status
+}
+
+type Status struct {
+	Code     int
+	Progress int
+}
+
+func New(dir string) *Partial {
+	err := os.MkdirAll(dir, os.ModeDir)
+	if err != nil {
+		panic(err)
+	}
+	return &Partial{map[string]*Storage{}, dir}
+}
+
+func NewTemp(dir string) *Partial {
+	return New(path.Join(os.TempDir(), dir))
+}
+
+func (p Partial) Session(session string) *Storage {
+	storage, ok := p.statuses[session]
+	if !ok {
+		err := os.MkdirAll(path.Join(p.dir, session), os.ModeDir)
+		if err != nil {
+			panic(err)
+		}
+		storage = &Storage{
+			p.dir,
+			session,
+			"",
+			Status{
+				CodeWaiting,
+				0,
+			},
+		}
+		p.statuses[session] = storage
+	}
+	return storage
+}
+
+func (s *Storage) StatusCode() int {
+	return s.status.Code
+}
+
+func (s *Storage) StatusProgress() int {
+	return s.status.Progress
+}
+
+func (s *Storage) ZipPath() string {
+	return s.zip
+}
+
+func (s *Storage) Create(file *multipart.FileHeader) string {
+	createdFile, err := os.Create(path.Join(s.dir, s.session, file.Filename))
+	defer func() {
+		err = createdFile.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	if err != nil {
+		panic(err)
+	}
+	open, err := file.Open()
+	if err != nil {
+		panic(err)
+	}
+	copyToFile(open, file.Size, createdFile)
+	err = open.Close()
+	if err != nil {
+		panic(err)
+	}
+	return file.Filename
+}
+
+func (s *Storage) Delete(filename string) error {
+	p := path.Join(s.dir, s.session, filename)
+	err := os.Remove(p)
+	if err != nil {
+		_, ok := err.(*os.PathError)
+		if !ok {
+			panic(err)
+		}
+		return errors.New("file does not exist")
+	}
+	return nil
+}
+
+func (s *Storage) Reset() {
+	*s = Storage{
+		dir:     s.dir,
+		session: s.session,
+		zip:     "",
+		status: Status{
+			Code:     CodeWaiting,
+			Progress: 0,
+		},
+	}
+}
+
+func copyToFile(src multipart.File, srcLength int64, dst *os.File) {
+	chunkSize := int64(1024)
+	buffer := make([]byte, chunkSize)
+	for i := int64(0); i*chunkSize < srcLength; i++ {
+		offset := i * chunkSize
+		_, err := src.ReadAt(buffer, offset)
+		if errors.Is(err, io.EOF) {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		_, err = dst.WriteAt(buffer, offset)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (s *Storage) Zip() {
+	s.status.Code = CodeRunning
+	zipName := path.Join(s.dir, s.session+"-result.zip")
+	create, err := os.Create(zipName)
+	if err != nil {
+		panic(err)
+	}
+	zipFile := zip.NewWriter(create)
+	s.writeRecursive(zipFile)
+	err = zipFile.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = create.Close()
+	if err != nil {
+		panic(err)
+	}
+	s.status.Code = CodeFinished
+	s.zip = zipName
+}
+
+func (s *Storage) writeRecursive(writer *zip.Writer) {
+	entries, err := os.ReadDir(path.Join(s.dir, s.session))
+	if err != nil {
+		panic(err)
+	}
+	total := len(entries)
+	for done, entry := range entries {
+		if entry.IsDir() {
+			panic("invalid state: inner directory detected")
+		} else {
+			header := &zip.FileHeader{}
+			header.Name = path.Join(entry.Name())
+			withHeader, err := writer.CreateHeader(header)
+			if err != nil {
+				panic(err)
+			}
+			file, err := os.ReadFile(path.Join(s.dir, s.session, entry.Name()))
+			if err != nil {
+				panic(err)
+			}
+			_, err = withHeader.Write(file)
+			if err != nil {
+				panic(err)
+			}
+			s.status.Progress = (done + 1) / total * 100
+		}
+	}
+}
